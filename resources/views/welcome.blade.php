963ebbaf4133bd371c347184834de87a07958f61
Ayantika Dey <devopsproject@outlook.com>
2017-12-28 16:35:26 +0530

    Modifying the web.config file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         }
         @font-face{
             font-family: "Segoe UI";
-            src: url('/public/fonts/segoeuil.ttf');
+            src: url('/fonts/segoeuil.ttf');
         }
         .main-container {
             height: 100%;
@@ -151,12 +151,12 @@
     <body>
         <div class="main-container">
         <div class="cloud-image">
-            <img src="/public/img/successCloudNew.svg" />
+            <img src="/img/successCloudNew.svg" />
         </div>
         <div class="content">
             <div class="tweet-container">
             <a href="http://twitter.com/intent/tweet/?text=I%20just%20created%20a%20new%20PHP%20Laravel%20website%20on%20Azure%20using%20Azure%20DevOps%20Project&hashtags=AzureDevOpsProject%2CVSTS%20%40Azure%20%40VSTS">
-                <img src="/public/img/tweetThis.svg" />
+                <img src="/img/tweetThis.svg" />
             </a>            
         </div>
             <div class="content-body">
@@ -168,25 +168,25 @@
                     <div class="next-steps-body">
                         <div class="step">
                             <div class="step-icon">
-                                <img src="/public/img/cloneWhite.svg">
+                                <img src="/img/cloneWhite.svg">
                             </div>
                             <div class="step-text"><a href="https://go.microsoft.com/fwlink/?linkid=862409">Clone your code repository and start developing your application on IDE of your choice</a></div>
                         </div>
                         <div class="step">
                             <div class="step-icon">
-                                <img src="/public/img/deployWhite.svg">
+                                <img src="/img/deployWhite.svg">
                             </div>
                             <div class="step-text"><a href="https://go.microsoft.com/fwlink/?linkid=862410">View your CI/CD pipeline on Visual Studio Team Services and customize it as per your needs</a></div>
                         </div>
                         <div class="step">
                             <div class="step-icon">
-                                <img src="/public/img/stackWhite.svg">
+                                <img src="/img/stackWhite.svg">
                             </div>
                             <div class="step-text"><a href="http://portal.azure.com">View your service stack in the Azure Portal</a></div>
                         </div>
                         <div class="step">
                             <div class="step-icon">
-                                <img src="/public/img/lightbulbWhite.svg">
+                                <img src="/img/lightbulbWhite.svg">
                             </div>
                             <div class="step-text"><a href="https://go.microsoft.com/fwlink/?linkid=862126">Learn more about all you can do with Azure projects by visiting the documentation</a></div>
                         </div>
